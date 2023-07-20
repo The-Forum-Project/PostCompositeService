bfa171b6686f2f68a0defbe12426fb9fb06f7611
Tongxuan Wang <tw2484@nyu.edu>
2023-07-20 10:04:09 -0700

    first commit

---
+++ src/main/java/com/example/postcompositeservice/PostCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.postcompositeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCompositeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostCompositeServiceApplication.class, args);
+    }
+
+}
